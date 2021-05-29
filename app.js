a693fe8c121216b8143fa38a48ee63d59f0310c3
Mahmoud Amane <mahmoudamane5@gmail.com>
2021-05-29 14:10:37 +0100

    :construction: Create users basic routes

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const morgan = require("morgan");
 
 const tourRouter = require("./routes/tourRoutes");
+const userRouter = require("./routes/userRoutes");
 
 const app = express();
 
@@ -25,5 +26,6 @@ app.use((req, res, next) => {
 
 // Routes
 app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
 
 module.exports = app;
