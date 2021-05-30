22bddecf1e40f8796dedd9804bc2f7c578c590ff
ma7ma7 <mahmoudamane5@gmail.comm>
2021-05-30 16:41:10 +0100

    :card_file_box: Connction to db

--- server.js
+++ server.js
@@ -1,12 +1,23 @@
-const dotenv = require("dotenv");
-const app = require("./app");
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const app = require('./app');
 
 dotenv.config({
-  path: "./config.env",
+  path: './config.env',
 });
 
+const DB = process.env.DATABASE_URL.replace('<PASSWORD>', process.env.PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then((con) => console.log('🗃  Database connection success ✓ '));
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-  console.log("🚀 Start lestning on port : 👂" + port);
+  console.log('🚀 Start lestning on port : 👂 ' + port);
 });
